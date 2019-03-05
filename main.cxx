a90ba5caa6ab52783c5eb9d7f09687f8e407b235
iloveooz <iloveooz@yandex.ru>
2019-03-05 16:54:25 +0700

    almost done

--- main.cxx
+++ main.cxx
@@ -2,6 +2,7 @@
 // Created by fahr on 04.03.19.
 //
 
+#include <iostream>
 #include <SFML/Graphics.hpp>
 #include <time.h>
 
@@ -16,8 +17,10 @@ int board[8][8] = {
          0,  0,  0,  0,  0,  0,  0,  0,
          0,  0,  0,  0,  0,  0,  0,  0,
          0,  0,  0,  0,  0,  0,  0,  0,
-        -6, -6, -6, -6, -6, -6, -6, -6,
-        -1, -2, -3, -4, -5, -3, -2, -1 };
+         6,  6,  6,  6,  6,  6,  6,  6,
+         1,  2,  3,  4,  5,  3,  2,  1 };
+
+std::string position = "";
 
 void loadPosition() {
     int k = 0;
@@ -34,9 +37,37 @@ void loadPosition() {
         }
 }
 
+std::string toChessNote(sf::Vector2f p) {
+    std::string s = "";
+    s += char(p.x / size + 97);
+    s += char(7 - p.y / size + 49);
+    return s;
+}
+
+sf::Vector2f toCoord(char a, char b) {
+    int x = int(a) - 97;
+    int y = 7 - int(b) + 49;
+    return sf::Vector2f(x * size, y * size);
+}
+
+void move (std::string str) {
+    sf::Vector2f oldPos = toCoord(str[0], str[1]);
+    sf::Vector2f newPos = toCoord(str[2], str[3]);
+
+    for (int i = 0; i < 32; i++) {
+        if (f[i].getPosition() == newPos)
+            f[i].setPosition(-100, -100);
+    }
+
+    for (int i = 0; i < 32; i++) {
+        if (f[i].getPosition() == oldPos)
+            f[i].setPosition(newPos);
+    }
+
+}
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(453, 453), "The Chess");
+    sf::RenderWindow window(sf::VideoMode(453, 454), "The Chess");
 
     sf::Texture t1, t2;
     t1.loadFromFile("images/figures.png");
@@ -54,6 +85,11 @@ int main() {
     float dx = 0;
     float dy = 0;
 
+    sf::Vector2f oldPos, newPos;
+    std::string str;
+
+    int n = 0;
+
 
     while (window.isOpen()) {
 
@@ -68,18 +104,29 @@ int main() {
             // drag and drop
             if (e.type == sf::Event::MouseButtonPressed)
                 if (e.key.code == sf::Mouse::Left)
-                    if (s.getGlobalBounds().contains(pos.x, pos.y)) {
-                        isMove = true;
-                        dx = pos.x - s.getPosition().x;
-                        dy = pos.y - s.getPosition().y;
-                    }
+                    for (int i = 0; i < 32; i++)
+                        if (f[i].getGlobalBounds().contains(pos.x, pos.y)) {
+                            isMove = true;
+                            n = i;
+                            dx = pos.x - f[i].getPosition().x;
+                            dy = pos.y - f[i].getPosition().y;
+                            oldPos = f[i].getPosition();
+                        }
             if (e.type == sf::Event::MouseButtonReleased)
-                if (e.key.code == sf::Mouse::Left)
+                if (e.key.code == sf::Mouse::Left) {
                     isMove = false;
+                    sf::Vector2f p = f[n].getPosition() + sf::Vector2f(size / 2, size / 2);
+                    sf::Vector2f newPos = sf::Vector2f(size * int(p.x / size), size *int(p.y / size));
+
+                    str = toChessNote(oldPos) + toChessNote(newPos);
+                    move(str);
+                    std::cout << str << std::endl;
+                    f[n].setPosition(newPos);
+                }
 
         }
 
-        if (isMove) s.setPosition(pos.x - dx, pos.y - dy);
+        if (isMove) f[n].setPosition(pos.x - dx, pos.y - dy);
 
         window.clear();
         window.draw(sBoard);
